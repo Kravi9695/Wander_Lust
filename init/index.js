12080d3df4716a021f973ac32a886a43b1558eb2
Ravi Kumar <ravi284206@gmail.com>
2024-08-02 22:46:43 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "66aa687caa9dd329a7b43696",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("Database initialized with sample data");
+};
+
+initDB();
